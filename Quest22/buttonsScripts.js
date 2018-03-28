ea1c4a65727178032cb049f8569e5fea1de6cec0
DarkFallenHunter <36505016+DarkFallenHunter@users.noreply.github.com>
2018-03-28 12:31:26 +0300

    Update buttonsScripts.js

--- Quest22/buttonsScripts.js
+++ Quest22/buttonsScripts.js
@@ -3,7 +3,7 @@ function randArr(maxLength)
   var len = Math.floor(Math.random() * maxLength)
   var arr = new Array(len > 4 ? len : 4)
   
-  for (var i = 0; i < len; i++)
+  for (var i = 0; i < arr.length; i++)
     arr[i] = Math.floor(Math.random() * 500)
     
   return arr
